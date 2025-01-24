52e6cc8c9390d448023086a0cb08766433306bea
Krzysztof Matczak <kmatczak@gmail.com>
2025-01-24 17:31:35 +0100

    Added signal handler for SIGINT and updated the gitignore file

--- standalone/src/smart_nta.c
+++ standalone/src/smart_nta.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "pkt_capture_ops.h"
+#include <stdlib.h>
 #include "hl_api.h"
+#include <signal.h>
 
-int register_hl_api(traffic_classifier *tc) {
+static traffic_classifier tc;
+
+int register_hl_api(traffic_classifier *tc)
+{
     // Register high-level API
     tc->start_traffic_classification = impl_start_traffic_classification;
     tc->stop_traffic_classification = impl_stop_traffic_classification;
@@ -10,37 +15,48 @@ int register_hl_api(traffic_classifier *tc) {
     return 0;
 }
 
+void handle_signal(int signal)
+{
+    // Stop traffic classification. In the final implementation, this will be called from the management
+    // interface or a control plane application using TR-181 data model.
+    tc.stop_traffic_classification();
+    //exit(0);
+}
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Initialize variables
     int ret = 0;
-    traffic_classifier tc;
     int sampling_window = 1000;
     int interval = 20000;
-    
+    struct sigaction sa;
+
     // Parse command-line arguments
 
     // Main program logic
     printf("Hello, this is Smart Network traffic Analyzer !\n");
 
     ret = register_hl_api(&tc);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         fprintf(stderr, "Failed to register high-level API. Error code: %d\n", ret);
         return ret;
     }
 
-    // Start traffic classification. In the final implementation, this will be called from the management 
+    // Set up signal handler for SIGKILL
+    sa.sa_handler = handle_signal;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1)
+    {
+        perror("Error setting up signal handler");
+        return 1;
+    }
+
+    // Start traffic classification. In the final implementation, this will be called from the management
     // interface or a control plane application using TR-181 data model.
     tc.start_traffic_classification("eth0", sampling_window, interval);
 
-
-    // Stop traffic classification. In the final implementation, this will be called from the management 
-    // interface or a control plane application using TR-181 data model.
-    tc.stop_traffic_classification();
-      
-
-
     return ret;
 }
-
-
